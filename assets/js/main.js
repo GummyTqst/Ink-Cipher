1ad3271167595448ccb4582a3e29193c5a7bab17
Tobias <neumsnn.tobias@gmail.com>
2025-05-22 09:20:44 +0200

    made header with some responsive

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+const burgerMenu = document.querySelector('.burger-menu');
+const mainNav = document.querySelector('.main-nav');
+
+burgerMenu.addEventListener('click', function() {
+    burgerMenu.classList.toggle('active');
+    mainNav.classList.toggle('active');
+})

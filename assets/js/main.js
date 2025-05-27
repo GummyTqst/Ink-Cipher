48fd16818757aef4ae152a7a3c1568084f819c47
Tobias <neumsnn.tobias@gmail.com>
2025-05-27 12:48:14 +0200

    made simple dialog/modal in collection

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,28 @@ burgerMenu.addEventListener('click', function() {
     burgerMenu.classList.toggle('active');
     mainNav.classList.toggle('active');
 })
+
+
+
+let dialog = document.getElementById('dialog');
+let dialogContent = document.getElementById('dialog-content');
+const showBtn = document.querySelectorAll('.show-dialog');
+const closeBtn = document.querySelector('.close-dialog');
+
+showBtn.forEach(function (btn) {
+    btn.addEventListener('click', function() {
+        let hiddenContent = this.querySelector('.modal-content');
+        
+        if (hiddenContent) {
+            dialogContent.innerHTML = hiddenContent.innerHTML;
+            dialog.showModal();
+        }
+    });
+})
+
+closeBtn.addEventListener('click', closeDialog);
+
+function closeDialog() {
+    dialog.close();
+    dialogContent.innerHTML = "";
+}
